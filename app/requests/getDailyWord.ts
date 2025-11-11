4552567eb4e8323d29a5e7d7386e0aa197d5e260
Zuriel Barrios <street_dark_fighter@hotmail.com>
2025-11-11 16:24:44 -0600

    normalize word

--- app/requests/getDailyWord.ts
+++ app/requests/getDailyWord.ts
@@ -3,11 +3,15 @@ import axios from "axios";
 const BASE_URL = 'https://rae-api.com/api'
 
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*, https://rae-api.com';
+function cleanWord(word: string): string {
+    return word.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase();
+}
 
 async function getDailyWord(): Promise<string> {
   const response = await axios.get(`${BASE_URL}/random`, { params: { max_length: 5, min_length: 5 } });
   const data = await response.data?.data;
-  return data.word;
+  const word = data.word;
+  return cleanWord(word);
 }
 
 export {
