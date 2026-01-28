df9be9800b84b9f7d78c7f3a74d3104d85a60566
Zuriel Barrios <street_dark_fighter@hotmail.com>
2026-01-28 16:40:44 -0600

    upgrade to 6 letters

--- clean_word_db.py
+++ clean_word_db.py
@@ -13,21 +13,21 @@ def normalize(s):
         s = s.replace(a, b).replace(a.upper(), b.upper())
     return s
 
-def get_5_letters_words(file_path):
+def get_6_letters_words(file_path):
     """
-    Reads a file and returns a list of words that are exactly 5 letters long.
+    Reads a file and returns a list of words that are exactly 6 letters long.
 
     :param file_path: Path to the text file containing words.
-    :return: List of 5-letter words.
+    :return: List of 6-letter words.
     """
-    five_letter_words = []
+    six_letter_words = []
     with open(file_path) as file:
         lines_list = file.readlines()
         for line in lines_list:
             word = line.strip()
-            if len(word) == 5:
+            if len(word) == 6:
                 word_encoded = normalize(word)
-                five_letter_words.append(word_encoded)
-    unique_five_letter_words = list(set(five_letter_words))
-    json.dump(unique_five_letter_words, open('five_letter_words.json', 'w', encoding='utf-8'), ensure_ascii=False)
-get_5_letters_words('0_palabras_todas.txt')
+                six_letter_words.append(word_encoded)
+    unique_six_letter_words = list(set(six_letter_words))
+    json.dump(unique_six_letter_words, open('six_letter_words.json', 'w', encoding='utf-8'), ensure_ascii=False)
+get_6_letters_words('0_palabras_todas.txt')
